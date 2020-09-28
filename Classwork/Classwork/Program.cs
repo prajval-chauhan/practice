8f7f48d5c427d946653367b4233324b33cb19893
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-09-28 08:17:04 +0530

    updated attendance calc algo

--- Classwork/Classwork/Program.cs
+++ Classwork/Classwork/Program.cs
@@ -8,13 +8,25 @@ namespace Classwork
         {
             Console.WriteLine("Welcome to the Employee wage computation program");
             var random = new Random();
-            var randomBool = random.Next(2);
-            String attendance;
-            if (randomBool == 0)
-                attendance = "Present";
-            else
-                attendance = "Absent";
-            Console.WriteLine("Employee is : " + attendance);
+            int leave = 0; int present = 0;
+           
+            for (int i=1; i<=30; i++)
+            {
+                var randomBool = random.Next(2);
+                switch(randomBool)
+                {
+                    case 0:
+                        leave = leave + 1;
+                        break;
+                    case 1:
+                        present = present + 1;
+                        break;
+                }
+                   
+                    
+            }
+                Console.WriteLine("Total Attendance = " + present +"Total Leaves = " + leave);
+           
         }
     }
 }
