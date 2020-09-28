3f675a6f0b9acd7e5cf7c19e77ebc31c8d4fb4c7
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-09-28 08:05:42 +0530

    Attendance of the employee

--- Classwork/Classwork/Program.cs
+++ Classwork/Classwork/Program.cs
@@ -7,6 +7,14 @@ namespace Classwork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee wage computation program");
+            var random = new Random();
+            var randomBool = random.Next(2);
+            String attendance;
+            if (randomBool == 0)
+                attendance = "Present";
+            else
+                attendance = "Absent";
+            Console.WriteLine("Employee is : " + attendance);
         }
     }
 }
